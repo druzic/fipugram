b3a80c0c891b982adaa9b95fdf86e904f948e0ae
druzic <ruzicdominik11@gmail.com>
2021-11-08 19:28:33 +0100

    vue_2

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <nav id="nav" class="navbar navbar-expand-lg navbar-light">
+      <a class="navbar-brand" href="#">
+        <img
+          src="@/assets/fipu_logo.png"
+          height="40"
+          class="d-inline-block align-top"
+          alt=""
+          loading="lazy"
+        />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarTogglerDemo01"
+        aria-controls="navbarTogglerDemo01"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarToggler">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item active">
+            <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/login" class="nav-link">Prijava</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/signup" class="nav-link"
+              >Registracija</router-link
+            >
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input
+            class="form-control mr-sm-2"
+            type="search"
+            placeholder="Pretraga"
+            aria-label="Search"
+          />
+        </form>
+      </div>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="row">
+    <div class="col-2"></div>
+    <div class="col-7">
+      <instagram-card />
+      <instagram-card />
+    </div>
+    <div class="col-3">Sidebar</div>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import InstagramCard from "@/components/InstagramCard.vue";
+
+export default {
+  name: "Home",
+  components: {
+    InstagramCard,
+  },
+};
+</script>
